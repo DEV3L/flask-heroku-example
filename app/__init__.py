559fc2aa65f558f9f62fac2ba5de189d8c703f7d
Justin Beall <jus.beall@gmail.com>
2018-03-01 12:59:36 -0500

    Add microblog flask application

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,5 @@
-# from flask import Flask
-#
-# app = Flask(__name__)
-#
-# from app import routes
-
-
 from flask import Flask
-from flask_runner import Manager
 
 app = Flask(__name__)
-manager = Manager(app)
 
-
-@app.route('/')
-def index():
-    return 'hello world!!!'
-
-
-if __name__ == '__main__':
-    manager.run()
+from app import routes
