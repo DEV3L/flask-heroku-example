1c0f6dd2ab1760a61caf4be2696223d922ec4288
Justin Beall <jus.beall@gmail.com>
2018-03-01 12:39:41 -0500

    Add app module

---
+++ _app.py
@@ -0,0 +1,14 @@
+# from flask import Flask
+# from flask_runner import Manager
+#
+# _app = Flask(__name__)
+# manager = Manager(_app)
+#
+#
+# @_app.route('/')
+# def index():
+#     return 'hello world!'
+#
+#
+# if __name__ == '__main__':
+#     manager.run()
--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,10 @@
+# from flask import Flask
+#
+# app = Flask(__name__)
+#
+# from app import routes
+
+
 from flask import Flask
 from flask_runner import Manager
 
@@ -7,7 +14,7 @@ manager = Manager(app)
 
 @app.route('/')
 def index():
-    return 'hello world'
+    return 'hello world!'
 
 
 if __name__ == '__main__':
