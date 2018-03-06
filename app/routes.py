0cdc6dc804c6530a61fbc87b42638667ab87f8a2
Justin Beall <jus.beall@gmail.com>
2018-03-06 13:53:49 -0500

    Add jinja2 template to route

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,25 @@
+from flask import render_template
+
 from app import app
+
+@app.route('/index1')
+def index1():
+    user = {'username': 'DEV3L'}
+    return '''
+    <html>
+        <head>
+            <title>Home Page - Microblog</title>
+        </head>
+        <body>
+            <h1>Hello, ''' + user['username'] + '''!</h1>
+        </body>
+    </html>'''
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!!!"
+    adventurer = {'hat': 'fedora',
+                  'name': 'DEV3L'}
+
+    games = [1,2,3,4,5]
+    return render_template('index.html', title='Dashboard', adventurer=adventurer, games=games)
