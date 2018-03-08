8301796aaa869580d0b19ceaaa2c0ce6d5b82ae1
Justin Beall <jus.beall@gmail.com>
2018-03-08 12:48:22 -0500

    Add jinja2 templates

--- app/routes.py
+++ app/routes.py
@@ -15,11 +15,31 @@ def index1():
         </body>
     </html>'''
 
-@app.route('/')
-@app.route('/index')
-def index():
+@app.route('/index2')
+def index2():
     adventurer = {'hat': 'fedora',
                   'name': 'DEV3L'}
 
     games = [1,2,3,4,5]
-    return render_template('index.html', title='Dashboard', adventurer=adventurer, games=games)
+    return render_template('index.html',
+                           # parameters sent to template
+                           title='', adventurer=adventurer, games=games)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Justin'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+
+    print('name = ' + posts[0]['author']['username'])
+    return render_template('index.html', title='Home', user=user, posts=posts)
